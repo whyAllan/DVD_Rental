93ee2d68d93ed9c66998c4b47b58fcb52991efff
Your Name <you@example.com>
2024-07-26 04:47:56 -0300

    .

--- DVD_RENTAL/settings.py
+++ DVD_RENTAL/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DVD_RENTAL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dvdrental',
+        'USER': 'postgres',
+        'PASSWORD': '71254267',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
